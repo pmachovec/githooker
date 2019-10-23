b50697ae922c984444650523ae9e8ed2838ae86e
Petr Machovec <pmachovec@outlook.com>
2019-10-23 19:02:25 +0200

    Ultrabuilder integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "9.0.0"
+    id("com.pmachovec.ultrabuilder").version("1.0")
 }
 
 group = "com.pmachovec"
@@ -26,7 +27,7 @@ dependencies {
     testImplementation("org.powermock", "powermock-module-testng", "2.0.2")
     testImplementation("org.testng", "testng", "7.0.0")
 
-    runtime(files(sourceSets["main"].output.resourcesDir))
+    runtimeOnly(files(sourceSets["main"].output.resourcesDir))
 }
 
 // PUBLICATION TO MAVEN REPOSITORY
